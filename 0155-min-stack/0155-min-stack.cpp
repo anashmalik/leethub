f2581e627f6a7d4a432773efd17fd14cd6d9887a
Anash Malik <117440315+anashmalik@users.noreply.github.com>
2024-01-03 19:29:39 +0530

    Time: 21 ms (35.15%), Space: 16.9 MB (22.25%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,43 +1,43 @@
 class MinStack {
 public:
-    stack<int>a,b;
-    MinStack() {
-        a=b;
+    typedef struct node{
+        int v;
+        int minUntilNow;
+        node* next;
+    }node;
+
+    MinStack() : topN(nullptr){
+        
     }
     
     void push(int val) {
-        a.push(val);
+        node* n = new node;
+        n->v = n->minUntilNow = val;
+        n->next = nullptr;
+        
+        if(topN == nullptr){
+            topN = n;
+        }
+
+        else{
+            n->minUntilNow = min(n->v,topN->minUntilNow);
+            n->next = topN;
+            topN = n;
+        }
     }
     
     void pop() {
-        a.pop();
+        topN = topN->next;
     }
     
     int top() {
-    return a.top();        
+        return topN->v;
     }
     
     int getMin() {
-        int x=INT_MAX;
-        while(!a.empty()){
-            x=min(a.top(),x);
-            b.push(a.top());
-            a.pop();
-        }
-        while(!b.empty()){
-            // x=min(a.top(),x);
-            a.push(b.top());
-            b.pop();
-        }
-        return x;
+        return topN->minUntilNow;
     }
-};
 
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack* obj = new MinStack();
- * obj->push(val);
- * obj->pop();
- * int param_3 = obj->top();
- * int param_4 = obj->getMin();
- */
+    private:
+    node* topN;
+};
