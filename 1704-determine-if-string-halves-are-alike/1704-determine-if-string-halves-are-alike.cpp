0fb6e3a71f1eaa5df89ded4420f2de80595b178a
Anash Malik <117440315+anashmalik@users.noreply.github.com>
2024-01-12 19:28:21 +0530

    Time: 11 ms (5.01%), Space: 6.9 MB (72.68%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     bool halvesAreAlike(string s) {
+        transform(s.begin(), s.end(), s.begin(), ::tolower); 
         int n=s.size(),i,t=0,y=0;
         for( i=0;i<n/2;i++){
             char x=s[i];
-            if(x=='a'||x=='i'||x=='e'||x=='o'||x=='u'||x=='A'||x=='E'||x=='I'||x=='O'||x=='U'){
+            if(x=='a'||x=='i'||x=='e'||x=='o'||x=='u'){
                 cout<<x<<" ";
                 t++;}
         }
        for(i;i<n;i++){
         char x=s[i];
-            if(x=='a'||x=='i'||x=='e'||x=='o'||x=='u'||x=='A'||x=='E'||x=='I'||x=='O'||x=='U'){
+            if(x=='a'||x=='i'||x=='e'||x=='o'||x=='u'){
                 cout<<x<<" ";
                 y++;}
        }
