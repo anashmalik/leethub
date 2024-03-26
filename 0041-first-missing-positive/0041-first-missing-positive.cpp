5b1ce0932ef91c18ba850b6075a439e6c5f05eb3
Anash Malik <117440315+anashmalik@users.noreply.github.com>
2024-03-26 22:35:23 +0530

    Time: 59 ms (20.52%), Space: 44.8 MB (26.01%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -6,8 +6,10 @@ public:
         while( i<n and nums[i]<1 )i++;
         int ans=1;
         while(i<n){
-            if(ans!=nums[i++])return ans;
+            if(ans-1==nums[i])ans--;
+            else if(ans!=nums[i])return ans;
             ans++;
+            i++;
         }
         return ans;
     }
