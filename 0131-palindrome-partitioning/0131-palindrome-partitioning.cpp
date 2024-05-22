083fce7c166c06a5d09708a6f5183ea20089c86a
Anash Malik <117440315+anashmalik@users.noreply.github.com>
2024-05-22 17:46:47 +0530

    Time: 91 ms (62.24%), Space: 79.2 MB (37.15%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,29 +1,35 @@
 class Solution {
 public:
-    vector<vector<string>>ans;
+vector<vector<string>>ans;
     vector<vector<string>> partition(string s) {
-     vector<string>a;
-     help(s,0,"",a);  
-     return ans; 
+        vector<vector<int>>d(s.size(),vector<int>(s.size(),0));
+        for(int i=0;i<s.size();i++){
+            for(int j=i;j<s.size();j++){
+                d[i][j]=pal(s,i,j);
+            }
+        }
+        vector<string>a;
+        help(s,0,a,d);
+        return ans;
     }
-    void help(string &s,int i,string t,vector<string>a){
-        if(i==s.size()){
-            if(t=="") ans.push_back(a);
-            return;
+    int pal(string &s,int i,int j){
+        while(i<j){
+            if(s[i++]!=s[j--])return 0;
         }
-        t+=s[i];
-        help(s,i+1,t,a);
-        if(pal(t)){
-            a.push_back(t);
-            help(s,i+1,"",a);
-        }
-       
+        return 1;
     }
-    bool pal(string a){
-        int i=0,j=a.size()-1;
-        while(i<=j){
-            if(a[i++]!=a[j--])return 0;
+    void help(const string &s,int i, vector<string>&a, vector<vector<int>>&d){
+        if(s.size()==i){
+            ans.push_back(a);
         }
-        return true;
+        else{
+            for(int j=i;j<s.size();j++){
+                if(d[i][j]){
+                    a.push_back(s.substr(i,j-i+1));
+                    help(s,j+1,a,d);
+                    a.erase(a.end()-1);
+                }
+            }
+        }
     }
 };
