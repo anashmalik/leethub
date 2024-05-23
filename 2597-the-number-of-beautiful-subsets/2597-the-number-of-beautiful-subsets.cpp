541c84abc8fd64565ab3b55ec63f9bb8e212532c
Anash Malik <117440315+anashmalik@users.noreply.github.com>
2024-05-23 16:35:58 +0530

    Time: 23 ms (74.59%), Space: 46.7 MB (6.61%) - LeetHub

--- 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.cpp
+++ 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    int beautifulSubsets(vector<int>& nums, int k) {
-        int n=nums.size();
-        vector<vector<vector<int>>>dp(2, vector<vector<int>>(n,vector<int>(n,-1)));
-        vector<int>a;
-       return help(nums,k,0,0,1,dp,a);
-    }
-    int help(vector<int>& nums, int &k,int i,int j,int f, vector<vector<vector<int>>>&dp, vector<int>&a){
-        if(i==nums.size()){
-           return a.size()!=0;}
-        int t=0;
-        t=help(nums,k,i+1,j,f,dp,a);
-        for(auto x:a){
-            if(abs(x-nums[i])==k)return t;
+    int helper(vector<int>& nums, int k) {
+        if (nums.size() <= 1) return nums.size()+1;
+       
+        vector<int> s;
+        int x = nums[nums.size()-1] - k;
+        for (int i = 0; i < nums.size() - 1; i++) {
+            if (nums[i] != x) {
+                s.push_back(nums[i]);
+            }
         }
-        a.push_back(nums[i]);
-       t+= help(nums,k,i+1,j,f,dp,a);
-       a.erase(a.end()-1);
-    return t;
+        nums.pop_back();
+        if (nums.size() == s.size()) {
+            return helper(nums, k) * 2;
+        } else {
+            return helper(nums, k) + helper(s, k);
+        }
     }
-    
-        
-    
+    int beautifulSubsets(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        return helper(nums, k) - 1;
+    }
 };
