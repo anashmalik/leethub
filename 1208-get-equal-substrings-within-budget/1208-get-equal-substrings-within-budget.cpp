fd593b7adf08f454ba4ef2aba147bc027fe9b425
Anash Malik <117440315+anashmalik@users.noreply.github.com>
2024-05-28 11:07:19 +0530

    Time: 4 ms (85.51%), Space: 9.5 MB (12.01%) - LeetHub

--- 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.cpp
+++ 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.cpp
@@ -5,12 +5,12 @@ public:
         for(int i=0;i<s.size();i++){
             v[i]=abs(s[i]-t[i]);
         }
-        int j=0,s=0,ans=0;
+        int j=0,su=0,ans=0;
         for(int i=0;i<s.size();i++){
-            s+=v[i];
-            if(s<=maxCost)ans=max(ans,i-j+1);
-            while(s>maxCost){
-                s-=v[j++];
+            su+=v[i];
+            if(su<=maxCost)ans=max(ans,i-j+1);
+            while(su>maxCost){
+                su-=v[j++];
             }
         }
         return ans;
