f2b3fa9c28ccbb01c204b245fc23fff2cb6f30ae
Anash Malik <117440315+anashmalik@users.noreply.github.com>
2024-07-10 13:50:56 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1598-crawler-log-folder/1598-crawler-log-folder.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        vector<string>a;
+        int t=0;
+        for(auto x:logs){
+            if(x=="../" and a.size())a.pop_back();
+            else if(x=="./")t++;
+            else a.push_back(x);
+        }
+        return a.size();
+    }
+};
