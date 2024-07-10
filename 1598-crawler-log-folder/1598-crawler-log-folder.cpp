3530f0ce3947aacb4c64865523814bd3c83fa17f
Anash Malik <117440315+anashmalik@users.noreply.github.com>
2024-07-10 13:51:01 +0530

    Time: 7 ms (26.85%), Space: 14.4 MB (17.13%) - LeetHub

--- 1598-crawler-log-folder/1598-crawler-log-folder.cpp
+++ 1598-crawler-log-folder/1598-crawler-log-folder.cpp
@@ -4,7 +4,9 @@ public:
         vector<string>a;
         int t=0;
         for(auto x:logs){
-            if(x=="../" and a.size())a.pop_back();
+            if(x=="../" ){
+                if( a.size())
+                a.pop_back();}
             else if(x=="./")t++;
             else a.push_back(x);
         }
