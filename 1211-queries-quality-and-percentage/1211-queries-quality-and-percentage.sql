93c963f5c94cec27e66dbaf1e9635175f7bc0941
Anash Malik <117440315+anashmalik@users.noreply.github.com>
2024-08-02 10:12:27 +0530

    Time: 393 ms (24.9%), Space: 0B (100%) - LeetHub

--- 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -1,1 +1,1 @@
-select query_name ,round((sum(rating /position )/count(*)),2) as quality,round((sum(if(rating <3,1,0))/count(*))*100,2)as poor_query_percentage from Queries  group by query_name 
+select query_name ,round((avg(rating/position )),2) as quality,round((sum(if(rating <3,1,0))/count(*))*100,2)as poor_query_percentage from Queries where query_name !='null' group by query_name 
