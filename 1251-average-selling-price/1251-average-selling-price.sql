adddb4050587ef080cc3118c652376253502bc53
Anash Malik <117440315+anashmalik@users.noreply.github.com>
2024-08-02 09:27:44 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1251-average-selling-price/1251-average-selling-price.sql
@@ -0,0 +1,1 @@
+select p.product_id, round(sum(p.price*u.units)/sum(u.units),2) as average_price  from UnitsSold u join Prices p on p.product_id = u.product_id  and u.purchase_date>=p.start_date and u.purchase_date<=p.end_date group by product_id 
