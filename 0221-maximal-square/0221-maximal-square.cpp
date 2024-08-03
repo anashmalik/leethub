384c8f8d3686287ed6246ca9e684a42ee5f7abac
Anash Malik <117440315+anashmalik@users.noreply.github.com>
2024-08-03 14:33:47 +0530

    Time: 58 ms (76.76%), Space: 24.5 MB (49.74%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
     int maximalSquare(vector<vector<char>>& mo) {
-        int m=mat.size();
-        int n=mat[0].size();
-       vector<vector<int>>mat(m,vector<int>(n,0));
-        for(int i=0;i<m;i++) mat[0][i]=(mo[0][i]=='1')?1:0;
-        
-
+        int m=mo.size();
+        int n=mo[0].size();
         int ans=0;
+       vector<vector<int>>mat(m,vector<int>(n,0));
+        for(int i=0;i<n;i++){ mat[0][i]=(mo[0][i]=='1')?1:0;
+        ans=max(ans,mat[0][i]);
+        }
+        for(int i=0;i<m;i++) {mat[i][0]=(mo[i][0]=='1')?1:0;
+        ans=max(ans,mat[i][0]);
+        }
+        
         for(int i=1;i<m;i++){
             for(int j=1;j<n;j++){
-               if(mat[i][j]=='1'){
+               if(mo[i][j]=='1'){
                 mat[i][j]=min(mat[i-1][j],min(mat[i][j-1],mat[i-1][j-1]))+1;
                 int t=mat[i][j];
                 ans = max(ans,t);
                }
             }
         }
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-              cout<<mat[i][j]<<" ";
-            }
-            cout<<endl;
-        }
-        return ans;
+       
+        return ans*ans;
     }
    
 };
